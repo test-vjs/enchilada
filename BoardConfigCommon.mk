5e584c72491ff03865bf14935d6f1b9e7cc2df5f
Christian Oder <myself5@carbonrom.org>
2018-06-27 21:14:43 -0400

    sdm845-common: Build disabled VBMeta image
    Change-Id: I381feef8f6fefc8449ca50d85d704b67bcc8a77e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,5 +138,9 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 # Verified Boot
 BOARD_BUILD_DISABLED_VBMETAIMAGE := true
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+
 # Inherit from the proprietary version
 -include vendor/oneplus/sdm845-common/BoardConfigVendor.mk
