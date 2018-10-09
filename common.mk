2e631b27dce0141453b39d6d87c5f20eadf1a7b6
Vachounet <vachounet@live.fr>
2018-10-09 16:16:24 +0200

    sdm845-common: set client package name
    * Introduce a new boot receiver that force camera client package name via HIDL HAL to fix portrait mode
    
    Change-Id: Ibac00612b794eefd3c6d7ba6fa4e44fcc0197cc1

--- common.mk
+++ common.mk
@@ -58,6 +58,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+# Camera
+PRODUCT_PACKAGES_DEBUG += \
+    CameraPackage
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
